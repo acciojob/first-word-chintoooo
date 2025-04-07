dce52f3fb30920f39a6da73a2d336813258b3b29
chintoooo <jeevansatishbonthu123@gmail.com>
2025-04-07 22:34:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+  // Trim leading spaces to handle cases like ' Hello World!'
+  str = str.trim();
+
+  // Find index of the first space
+  const spaceIndex = str.indexOf(" ");
+
+  // If there's no space, return the entire string
+  if (spaceIndex === -1) return str;
+
+  // Otherwise, return from 0 to the space index
+  return str.substring(0, spaceIndex);
 }
-
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
